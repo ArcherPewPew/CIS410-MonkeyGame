b6fe67c76a6a7d06a89acb1bb3b1d947acb530fa
Alex Archer <40580104+ArcherPewPew@users.noreply.github.com>
2021-06-03 23:11:39 -0700

    more tweaks
    more tweaks to enemy chase

--- Monkey Game/Assets/Scripts/EnemyBehavior.cs
+++ Monkey Game/Assets/Scripts/EnemyBehavior.cs
@@ -8,6 +8,7 @@ public class EnemyBehavior : MonoBehaviour
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
     public GameObject player;
+    public Animator animator;
 
     int m_CurrentWaypointIndex;
     public float range = 15.0f;
@@ -15,17 +16,34 @@ public class EnemyBehavior : MonoBehaviour
     void Start()
     {
         navMeshAgent.SetDestination(waypoints[0].position);
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
 
-        if(Vector3.Distance(navMeshAgent.nextPosition, player.transform.position) <= range) {
+        if(InRange()) {
             navMeshAgent.SetDestination(player.transform.position);
+            if (InAttackRange())
+            {
+                //play attack animation
+                //do attack stuff?
+            }
         } else if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
             m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
             navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
         }
+    }
+
+    bool InRange()
+    {
+        return Vector3.Distance(navMeshAgent.nextPosition, player.transform.position) <= range;
+    }
+
+    bool InAttackRange()
+    {
+        return Vector3.Distance(navMeshAgent.nextPosition, player.transform.position) <= 5;
+
     }
 }
